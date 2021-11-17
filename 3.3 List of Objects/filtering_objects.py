cadc6e6905b6a047ae75ff93cb1e923e4cbdfbb8
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-11-16 20:55:34 -0500

    Create filtering_objects.py

---
+++ 3.3 List of Objects/filtering_objects.py
@@ -0,0 +1,48 @@
+# UML
+#       Student
+# ---------------------
+# name: str
+# grade: int
+# student_id: int
+
+class Student:
+    def __init__(self, name: str, grade: int, student_id: int) -> None:
+        self.name = name
+        self.grade = grade
+        self.student_id = student_id
+
+    
+
+def filter_same_grade(students: list[Student], same_grade: int) -> list[Student]:
+    """filters students in the same grade
+
+    Args:
+        students: list[Student]
+        grade: int
+
+    Returns:
+        List of studnets in the grade
+    """
+    filtered = []
+    for stud in students:
+        if stud.grade == same_grade:
+            filtered.append(stud)
+
+    return filtered
+
+students = [
+    Student("Michael", 12, 12345),
+    Student("Bob", 12, 24681),
+    Student("Jack", 10, 36478),
+    Student("Will", 9, 12563),
+    Student("Mark", 10, 24635),
+    Student("Christina", 11, 32569),
+    Student("Olivia", 9, 25489),
+    Student("Joe", 12, 36598),
+    Student("Nick", 9, 26548)
+]
+
+
+same_grade = int(input("Enter Grade: "))
+result = filter_same_grade(students, same_grade)
+print(*result, sep="\n")
