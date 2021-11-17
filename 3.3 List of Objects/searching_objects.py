7c1b06c34d69e33ad499eb3a51ed0b8e0c8a0390
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-11-16 20:22:38 -0500

    Create searching_objects.py

---
+++ 3.3 List of Objects/searching_objects.py
@@ -0,0 +1,46 @@
+# UML
+#       Student
+# ---------------------
+# name: str
+# grade: int
+# student_id: int
+
+
+class Student:
+    def __init__(self, name: str, grade: int, student_id: int) -> None:
+        self.name = name
+        self.grade = grade
+        self.student_id = student_id
+
+def find_student(students: list[Student], student_id: int) -> str:
+    for stud in students:
+        if stud.student_id == student_id:
+            return stud
+        else:
+            stud = "None"
+            return stud
+
+students = [
+    Student("Michael", 12, 12345),
+    Student("Bob", 12, 24681),
+    Student("Jack", 10, 36478),
+    Student("Will", 9, 12563)
+]
+
+run = True
+while run == True:
+    id = int(input("Enter student id: "))
+    found = find_student(students, id)
+    if found == "None":
+        print("INVALID STUDENT ID")
+    else:
+        print(f"Name: {found.name} / Grade: {found.grade} / Student ID: {found.student_id}")
+    print()
+    print("Would you like to search for another student?")
+    print("[1]- Yes")
+    print("[2]- No")
+    again = int(input("-> "))
+    if again == 2:
+        run = False
+            
+print("Bye")
